4a6b9a4c1812379a94f9328001b41bcbe60e6554
Victor Hugo <victor.h.santos@ufv.br>
2021-12-19 18:35:39 -0300

    criado cadastro de moradores

--- iRep/src/irep/Morador.java
+++ iRep/src/irep/Morador.java
@@ -7,45 +7,45 @@
 
 package irep;
 
-import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Morador {
     
     private String nome, apelido, curso;
-    private Date dataNascimento, dataEntrada;
+    private LocalDate dataNascimento, dataEntrada;
     private int idade;
     //private Tarefa tarefaAtribuida;
+    
+    // Getters
+    public String getNome(){ return this.nome; }
+    public String getApelido(){ return this.apelido; }
+    public String getCurso(){ return this.curso; }
+    public LocalDate getDataNascimento(){ return this.dataNascimento; }
+    public LocalDate getDataEntrada(){ return this.dataEntrada; }
+    public int getIdade(){ return this.idade; }
     
     // Setters
     public void setNome(String nome){ this.nome = nome; }
     public void setApelido(String apelido){ this.apelido = apelido; }
     public void setCurso(String curso){ this.curso = curso; }
-    public void setDataNascimento(Date dataNascimento){ this.dataNascimento = dataNascimento; }
-    public void setDataEntrada(Date dataEntrada){ this.dataEntrada = dataEntrada; }
-    /*public void setIdade(Date dataNascimento, Date dataAtual){
-        int anoNascimento, anoAtual;
-        
-        anoNascimento = 
-        anoAtual = 
-        
-        this.idade = idade;
-    }*/
+    public void setDataNascimento(LocalDate dataNascimento){ this.dataNascimento = dataNascimento; }
+    public void setDataEntrada(LocalDate dataEntrada){ this.dataEntrada = dataEntrada; }
+    public void setIdade(LocalDate dataNascimento, LocalDate dataAtual){
+        this.idade = dataAtual.getYear() - dataNascimento.getYear();
+    }
     
-    public Morador(String nome, String apelido, String curso, Date dataNascimento){        
+    public Morador(String nome, String apelido, String curso, LocalDate dataNascimento){
+        LocalDate dataAtual;
+        
         this.setNome(nome);
         this.setApelido(apelido);
         this.setCurso(curso);
         this.setDataNascimento(dataNascimento);        
         // atribui data atual da execução
-        Date dataAtual = new Date(System.currentTimeMillis());   // coleta dados da data atual
+        dataAtual = LocalDate.now();
         this.setDataEntrada(dataAtual); // dataAtual será considerada a data de entrada
-        //this.setIdade(dataNascimento, dataAtual);       
+        this.setIdade(dataNascimento, dataAtual);       
     }    
-    
-    //public String getNome(){ return this.nome; }
-    
-    
-    
-    
 }
