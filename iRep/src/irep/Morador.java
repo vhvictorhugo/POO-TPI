211cf0430b388827eb7f2d42e0532cb55ddb4288
Victor Hugo <victor.h.santos@ufv.br>
2021-12-19 17:17:41 -0300

    criada classe morador

--- iRep/src/irep/Morador.java
+++ iRep/src/irep/Morador.java
@@ -1,5 +1,51 @@
+/*
+*   Projeto iRep - um jeito diferente de morar em repúblicas
+*   Autores: Lucas Takeshi (????) e Victor Hugo (3510)
+*   Matéria: CCF 313 - Programação Orientada a Objetos
+*   Professor: Fabrício Aguiar Silva
+*/
+
 package irep;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 public class Morador {
+    
+    private String nome, apelido, curso;
+    private Date dataNascimento, dataEntrada;
+    private int idade;
+    //private Tarefa tarefaAtribuida;
+    
+    // Setters
+    public void setNome(String nome){ this.nome = nome; }
+    public void setApelido(String apelido){ this.apelido = apelido; }
+    public void setCurso(String curso){ this.curso = curso; }
+    public void setDataNascimento(Date dataNascimento){ this.dataNascimento = dataNascimento; }
+    public void setDataEntrada(Date dataEntrada){ this.dataEntrada = dataEntrada; }
+    /*public void setIdade(Date dataNascimento, Date dataAtual){
+        int anoNascimento, anoAtual;
+        
+        anoNascimento = 
+        anoAtual = 
+        
+        this.idade = idade;
+    }*/
+    
+    public Morador(String nome, String apelido, String curso, Date dataNascimento){        
+        this.setNome(nome);
+        this.setApelido(apelido);
+        this.setCurso(curso);
+        this.setDataNascimento(dataNascimento);        
+        // atribui data atual da execução
+        Date dataAtual = new Date(System.currentTimeMillis());   // coleta dados da data atual
+        this.setDataEntrada(dataAtual); // dataAtual será considerada a data de entrada
+        //this.setIdade(dataNascimento, dataAtual);       
+    }    
+    
+    //public String getNome(){ return this.nome; }
+    
+    
+    
     
 }
--- iRep/src/irep/Principal.java
+++ iRep/src/irep/Principal.java
@@ -1,7 +1,67 @@
+/*
+*   Projeto iRep - um jeito diferente de morar em repúblicas
+*   Autores: Lucas Takeshi (????) e Victor Hugo (3510)
+*   Matéria: CCF 313 - Programação Orientada a Objetos
+*   Professor: Fabrício Aguiar Silva
+*/
+
 package irep;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
 
 public class Principal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{        
+        Principal principal = new Principal();
+        List<Morador> moradores = new LinkedList<Morador>();
+
+        principal.cadastraMorador(moradores);
+        //principal.editaMorador(moradores);        
     }
+    
+    public void cadastraMorador(List moradores)throws Exception {
+        System.out.println("---------- CADASTRO DE MORADORES ----------");
+        
+        int qtMoradores;
+        String nomeLeitura, dataNascimentoLeitura;
+        
+        Scanner scan = new Scanner(System.in); 
+        
+        System.out.print("Entre com a quantidade de moradores a cadastrar: ");        
+        
+        qtMoradores = scan.nextInt();
+        
+        for (int i = 0; i < qtMoradores; i++) {
+            
+            System.out.println("---------- MORADOR "+(i+1)+" ----------");
+            System.out.print("Entre com o nome do Morador: ");
+            nomeLeitura = scan.nextLine();
+            System.out.print("Entre com a data de nascimento (dd/MM/yyyy): ");
+            dataNascimentoLeitura = scan.nextLine();
+            
+            // converte data do tipo 'String' em data do tipo 'Date'
+            Date dataNascimento = new SimpleDateFormat("dd/MM/yyyy").parse(dataNascimentoLeitura);
+            
+            //Morador morador = new Morador(nomeLeitura, dataNascimento);            
+            //moradores.add(morador);
+        }
+    }
+    
+    /*public void editaMorador(List moradores){
+        System.out.println("---------- EDITAR MORADORES ----------");
+        
+        String nomeMorador;
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Entre com o nome do morador a editar: ");
+        nomeMorador = scan.nextLine();
+        
+        moradores.contains(moradores);
+        
+    }*/
 }
